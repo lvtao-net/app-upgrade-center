1588a5d091aa87d06a2ef624913d0fb51aba570e
滔哥 <admin@lvtao.net>
2025-03-31 19:27:51 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -13,36 +13,59 @@ try {
                 username TEXT UNIQUE NOT NULL,
                 password TEXT NOT NULL
             );
-            CREATE TABLE apps (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                package_name TEXT NOT NULL,
-                logo TEXT,
-                name TEXT NOT NULL,
-                description TEXT,
-                summary TEXT,
-                email TEXT,
-                com TEXT,
-                company TEXT,
-                screenshots TEXT,
-                rating TEXT DEFAULT '4.9',
-                category TEXT DEFAULT '工具',
-                age TEXT DEFAULT '4+',
-                status INTEGER DEFAULT 1,
-                created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+            CREATE TABLE logs (
+                id                INTEGER primary key autoincrement,
+                appid             INTEGER,
+                platform          TEXT,
+                version           TEXT,
+                header            TEXT,
+                info              TEXT,
+                created_at        INTEGER,
+                ip_address        TEXT,
+                user_agent        TEXT,
+                request_uri       TEXT,
+                host              TEXT,
+                content_type      TEXT,
+                server_protocol   TEXT,
+                https             TEXT,
+                device_brand      TEXT,
+                device_model      TEXT,
+                os_version        TEXT,
+                app_language      TEXT,
+                screen_width      INTEGER,
+                screen_height     INTEGER,
+                status_bar_height INTEGER,
+                safe_area         TEXT,
+                uni_version       TEXT
             );
-            CREATE TABLE app_versions (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                app_id INTEGER NOT NULL,
-                version TEXT NOT NULL,
-                ios_version TEXT NOT NULL,
-                changelog TEXT,
-                apk_url TEXT,
-                ios_store_url TEXT,
-                ipa_url TEXT,
-                force INTEGER DEFAULT 0,
-                created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (app_id) REFERENCES apps(id) ON DELETE CASCADE
+            CREATE TABLE logs(
+                id                INTEGER
+                    primary key autoincrement,
+                appid             INTEGER,
+                platform          TEXT,
+                version           TEXT,
+                header            TEXT,
+                info              TEXT,
+                created_at        INTEGER,
+                ip_address        TEXT,
+                user_agent        TEXT,
+                request_uri       TEXT,
+                host              TEXT,
+                content_type      TEXT,
+                server_protocol   TEXT,
+                https             TEXT,
+                device_brand      TEXT,
+                device_model      TEXT,
+                os_version        TEXT,
+                app_language      TEXT,
+                screen_width      INTEGER,
+                screen_height     INTEGER,
+                status_bar_height INTEGER,
+                safe_area         TEXT,
+                uni_version       TEXT
             );
+            CREATE TABLE apps (id INTEGER PRIMARY KEY, package_name TEXT NOT NULL, logo TEXT, name TEXT NOT NULL, description TEXT, summary TEXT, status INTEGER DEFAULT 1, created_at DATETIME DEFAULT 'CURRENT_TIMESTAMP', rating TEXT DEFAULT '4.9', category TEXT DEFAULT '工具', age TEXT DEFAULT '4+', screenshots TEXT, com TEXT, company TEXT, email TEXT, s_google TEXT, s_huawei TEXT, s_honor TEXT, s_mi TEXT, s_qq TEXT, s_vivo TEXT, s_oppo TEXT, s_apple TEXT);
+            CREATE TABLE app_versions (id INTEGER PRIMARY KEY, app_id INTEGER, version TEXT, apk_url TEXT, ipa_url TEXT, created_at DATETIME DEFAULT CURRENT_TIMESTAMP, changelog TEXT, ios_version TEXT, force INTEGER);
         ");
 
         // 添加默认管理员
